bb8a80bc2f6229e769ca7ea975b28fed3ddb85fb
YamaShuhei <plg.function06@gmail.com>
2023-04-24 10:57:29 +0900

    fix

--- src/app/Http/Controllers/PostController.php
+++ src/app/Http/Controllers/PostController.php
@@ -91,7 +91,6 @@ class PostController extends Controller
 
               $post->title=$request->input('title');
               $post->description=$request->input('description');
-              $post->image=$request->input('image');
               $post->save();
               
             //更新後投稿一覧へ遷移
